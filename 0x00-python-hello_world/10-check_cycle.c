ef2f599f173f83d18208a9259650e653baed93d3
DAROT11 <ojojamesayanfe@gmail.com>
2023-01-06 17:07:39 -0800

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
- *  * check_cycle - checks if a singly linked list has a cycle in it
- *   * @list: points to a struct of type listint_t
- *    * Return: 0 if there is no cycle, 1 if there is a cycle
- *     */
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: points to a struct of type listint_t
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
 int check_cycle(listint_t *list)
 {
 		listint_t *slow = list;
